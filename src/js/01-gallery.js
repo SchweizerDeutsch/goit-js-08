1aa3d248c843fb2b7cef5adf69519fca2347bb75
Ismail Aliev <aliev12ismail@gmail.com>
2023-08-02 13:34:09 +0200

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const items = [];
+
+galleryItems.forEach((element) => {
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__link";
+  galleryLink.href = element.original;
+  const galleryImage = document.createElement("img");
+  galleryImage.className = "gallery__image";
+  galleryImage.src = element.preview;
+  galleryImage.setAttribute("title", element.description);
+  galleryImage.alt = element.description;
+
+  galleryLink.append(galleryImage);
+  items.push(galleryLink);
+});
+gallery.append(...items);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
