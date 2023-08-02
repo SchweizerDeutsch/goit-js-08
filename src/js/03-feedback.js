bccec0685592465d7844086260bf8f1a4fa6c3a2
Ismail Aliev <aliev12ismail@gmail.com>
2023-08-02 17:20:38 +0200

    third commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from "lodash.throttle";
 
+function saveFormData() {
+    const emailInput = document.querySelector('input[name="email"]')
+    const messageInput = document.querySelector('textarea[name="message"]')
+
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value
+    }
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
+}
+
+const feedbackFormData = document.querySelector(".feedback-form")
+feedbackFormData.addEventListener("input", throttle(saveFormData, 500))
+
+function loadFormData() {
+    const storerData = localStorage.getItem("feedback-form-state")
+    if (storerData){
+        const formData = JSON.parse(storerData)
+        const emailInput = document.querySelector('input[name="email"]')
+        const messageInput = document.querySelector('textarea[name="message"]')
+
+        emailInput.value = formData.email
+        messageInput.value = formData.message
+    }
+}
+
+window.addEventListener("load", loadFormData)
+
+function handleSubmit(event){
+    event.preventDefault()
+    const storerData = localStorage.getItem("feedback-form-state")
+    if(storerData){
+        const formData = JSON.parse(storerData)
+        console.log(storerData)
+    }
+
+    localStorage.removeItem("feedback-form-state")
+    event.target.reset()
+}
+
+feedbackFormData.addEventListener("submit", handleSubmit)
